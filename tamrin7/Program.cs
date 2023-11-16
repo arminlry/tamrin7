d6c3d8b6684deeb493f0451285fa4f06319fb80c
armin liryaee <149113663+arminlry@users.noreply.github.com>
2023-11-17 00:05:15 +0330

    Update Program.cs

--- tamrin7/Program.cs
+++ tamrin7/Program.cs
@@ -1,6 +1,7 @@
-﻿//armin liryaee
+//armin liryaee
 //tamrin7
-// فقط اپشن های 1و2و3و4و6 درست و کامل هستند
+using System.Globalization;
+
 List<Student> students = new List<Student>();
 StreamReader sr = new StreamReader(@"C:\Users\Persian\Desktop\vs s\tamrin7\name.txt");
 while (!sr.EndOfStream)
@@ -73,7 +74,7 @@ do
     {
         foreach (var student in students)
 
-            Console.WriteLine(student.Fname + " " + student.Lname + " " + student.sortmianginkole());
+            Console.WriteLine(student.Fname + " " + student.Lname + " " + student.sortmianginkol(student.TotalAveragekol()));
     }
     else if (x == 8)
     {
@@ -92,40 +93,16 @@ do
     }
     else if (x == 11)
     {
-        int score = 0;
-        switch (score)
-        {
-            case int n when (n >= 17 && n <= 20):
-                Console.WriteLine("A");
-                break;
-            case int n when (n >= 15 && n < 17):
-                Console.WriteLine("B");
-                break;
-            case int n when (n >= 13 && n < 15):
-                Console.WriteLine("C");
-                break;
-            case int n when (n >= 10 && n < 13):
-                Console.WriteLine("D");
-                break;
-            case int n when (n >= 7 && n < 10):
-                Console.WriteLine("E");
-                break;
-            case int n when (n >= 3 && n < 7):
-                Console.WriteLine("F");
-                break;
-            case int n when (n >= 0 && n < 3):
-                Console.WriteLine("G");
-                break;
-            default:
-                Console.WriteLine("Invalid score");
-                break;
-        }
+        foreach (var student in students)
+        { Console.WriteLine(student.Fname + " " + student.Lname + " " + student.GradeExp(student.TotalAveragekol())); }
 
     }
 
 
     Console.WriteLine("darkhast digari darid?(y=bale)");
 } while (Console.ReadLine() == "y");
+
+
 class Student
 {
     public string Fname { get; set; }
@@ -140,15 +117,17 @@ class Student
     public double tarbiatbadani { get; set; }
     public double zabankhareji { get; set; }
     public double tafsir { get; set; }
+    public double TotalAveragekol()
+    { return ((riazi * 2 + zabankhareji * 2 + kargah + tarbiatbadani + tafsir + systemamel * 3 + algoritm * 3 + pishrafte1 * 3 + pishrafte2 * 3 + sheygaraei * 3) / 22); }
     public double TotalAveragedorus2setare()
     { return ((pishrafte1 * 3 + pishrafte2 * 3 + sheygaraei * 3) / 9); }
     public double TotalAveragedorus1setare()
     { return ((systemamel * 3 + algoritm * 3) / 6); }
     public double TotalAveragedorus()
     { return ((riazi * 2 + zabankhareji * 2 + kargah + tarbiatbadani + tafsir) / 7); }
-    public double TotalAveragekol()
-    { return ((riazi * 2 + zabankhareji * 2 + kargah + tarbiatbadani + tafsir + systemamel * 3 + algoritm * 3 + pishrafte1 * 3 + pishrafte2 * 3 + sheygaraei * 3) / 22); }
-    /* public double taeinadadaval()
+
+
+    /*public double taeinadadaval()
      {
          foreach (var pishrafte1 in Student)
              for (int i = 2; i < pishrafte1; i++)
@@ -164,17 +143,18 @@ class Student
              }
 
      }*/
-    public double sortmianginkole()
+
+    public double sortmianginkol(double results)
     {
-        double numbers = ((riazi * 2 + zabankhareji * 2 + kargah + tarbiatbadani + tafsir + systemamel * 3 + algoritm * 3 + pishrafte1 * 3 + pishrafte2 * 3 + sheygaraei * 3) / 22);
-        //double[] number = new double[];
-        //Array.Sort(number);
-        //foreach (double number in numbers)
-        //{
-        //    Console.WriteLine(number);
-        //}
-        return numbers;
+
+       // double results = TotalAveragekol();
+
+       // Array.Sort(results);
+        double averages = results;
+             return averages;                    
     }
+
+    
     public double sortmiangindorus2()
     {
         { return ((riazi * 2 + zabankhareji * 2 + kargah + tarbiatbadani + tafsir + systemamel * 3 + algoritm * 3 + pishrafte1 * 3 + pishrafte2 * 3 + sheygaraei * 3) / 22); }
@@ -187,6 +167,17 @@ class Student
     {
         { return ((riazi * 2 + zabankhareji * 2 + kargah + tarbiatbadani + tafsir + systemamel * 3 + algoritm * 3 + pishrafte1 * 3 + pishrafte2 * 3 + sheygaraei * 3) / 22); }
     }
-    public double gradebandi()
-    { return ((riazi * 2 + zabankhareji * 2 + kargah + tarbiatbadani + tafsir + systemamel * 3 + algoritm * 3 + pishrafte1 * 3 + pishrafte2 * 3 + sheygaraei * 3) / 22); }
+    public char GradeExp(double student) => TotalAveragekol() switch
+    {
+        double n when n > 20 || n < 0 => throw new ArgumentOutOfRangeException(),
+        >= 17 => 'A',                            
+        >= 15 => 'B',                            
+        >= 13 => 'C',                            
+        >= 10 => 'D',                            
+        >= 7 => 'E',                             
+        >= 3 => 'F',                             
+        >= 0 => 'G',                             
+        _ => throw new ArgumentOutOfRangeException()
+    };
+
 }
